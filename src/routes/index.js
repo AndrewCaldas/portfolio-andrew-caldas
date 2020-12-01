c9614ff956d5234db7b2a42a4254373579371e03
Andrew Caldas <andrewaguiar@ibm.com>
2020-11-30 23:29:16 -0300

    fix texts, etc..

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Portfolio from "../pages/Portfolio";
 import Contact from "../pages/Contact";
 import Projects from "../pages/Projects";
-// import error404 from "../share/error404";
+import error404 from "../share/error404";
 
 const Router = () => (
   <BrowserRouter>
@@ -16,12 +16,12 @@ const Router = () => (
         }}
       />
       <Route exact path="/portfolio" component={Portfolio} />
-      <Route exact path="/about" component={Portfolio} />
+      {/* <Route exact path="/about" component={Portfolio} /> */}
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/project/:id" component={Projects} />
 
       {/* page 404 */}
-      {/* <Route exact path="*" component={error404} /> */}
+      <Route exact path="*" component={error404} />
     </Switch>
   </BrowserRouter>
 );
