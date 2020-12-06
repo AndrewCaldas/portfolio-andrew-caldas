11ad6b75129c1968360c8b3b0c00024d5d5a8375
Andrew Caldas <andrewaguiar@ibm.com>
2020-12-06 19:12:54 -0300

    fine adjustments of css

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -4,23 +4,31 @@ import Footer from "../../components/Footer";
 import Drawings from "../../components/Drawings";
 import TextBold from "../../components/Text/TextBold";
 import TextLight from "../../components/Text/TextLight";
+import { Row, Col, Container } from "react-bootstrap";
 
 import "./index.css";
 
 const Contact = () => {
   return (
     <div className="page-contact-main">
-      <Header background={false} />
-      <Drawings
-        title={"Contato"}
-        description={"Abaixo você poderá entrar em contato comigo!"}
-        background={true}
-      />
+      <Header background={true} />
 
-      <div className="section-contact">
-        <TextLight text={"(11) 94573-5656"} color={"#000"} size={"medium"} />
-        <TextLight text={"contandrewcaldas@gmail.com"} color={"#000"} size={"medium"} />
-      </div>
+      <Row className="row-line">
+        <Col xs={12} style={{ marginTop: "100px" }}>
+          <Drawings
+            title={"Contato"}
+            description={"Abaixo você poderá entrar em contato comigo!"}
+            background={true}
+          />
+        </Col>
+
+        <Col xs={12}>
+          <div className="section-contact">
+            <TextLight text={"(11) 94573-5656"} color={"#000"} size={"medium"} />
+            <TextLight text={"contandrewcaldas@gmail.com"} color={"#000"} size={"medium"} />
+          </div>
+        </Col>
+      </Row>
 
       <Footer />
     </div>
